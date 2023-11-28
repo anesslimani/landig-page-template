c5f0cad89146cdd63938f4450d0088777c9bcf3b
anes <anesslimani9@gmail.com>
2023-11-29 00:16:19 +0100

    header for mobile and small screen

---
+++ src/header.js
@@ -0,0 +1,19 @@
+/*=============== SHOW MENU ===============*/
+const header = () => {
+
+    const navMenu = document.getElementById('nav-menu'),
+        navToggle = document.getElementById('nav-toggle'),
+        navClose = document.getElementById('nav-close')
+
+    /* Menu show */
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+
+    });
+    /* Menu hidden */
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+
+};
+export default header ;
---
+++ src/main.js
@@ -0,0 +1,5 @@
+import '../style/modern-normalizes.css';
+import '../style/style.css';
+import '../style/header.css';
+import header from '../src/header.js';
+header()
